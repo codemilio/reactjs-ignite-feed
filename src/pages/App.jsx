f64f14f426f780d76edfa6c625c90337713a9d12
Carlos Silva <carlosemilio.contato@gmail.com>
2024-05-24 00:19:37 -0300

    fix: mobile responsiveness

---
+++ src/pages/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from '../components/Header'
+import { PostList } from '../components/PostList'
+import { Sidebar } from '../components/Sidebar'
+import { Wrapper } from '../components/Wrapper'
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Wrapper>
+        <Sidebar />
+        <PostList />
+      </Wrapper>
+    </>
+  )
+}
+
